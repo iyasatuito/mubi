719696cfe26bfb33551cdf6994b14b6dabb6b3eb
Betina Bondoc <betinabondoc@gmail.com>
2021-10-10 15:57:08 +1100

    Environment setup

---
+++ admin/home.php
@@ -0,0 +1,9 @@
+<!-- include header file -->
+<?php include 'partials/admin-header.php'; ?>
+
+<div id="home">
+    
+</div>
+
+<!-- include footer file -->
+<?php include 'partials/admin-footer.php'; ?>
